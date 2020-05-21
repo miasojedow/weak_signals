211f9ced9691a52837042d1c7ed6c75f81b0d28e
bmia <bmia@mimuw.edu.pl>
2020-05-21 13:48:50 +0200

    last version

--- weak_signals.py
+++ weak_signals.py
@@ -4,15 +4,28 @@ import numpy as np
 from sklearn.decomposition import SparsePCA
 
 #Generate covariance matrix
+p=1000
+n=100
+k=5
+c=1
+Number_of_blocks = 100
 
-Number_of_blocks = 10
-blocks_size = np.array([4,4,5,5,6,6,7,7,8,8])
-ncol = np.sum(blocks_size)
+blocks_size = np.zeros(Number_of_blocks)
+used =0 
+for i in range(Number_of_blocks-1):
+  theta = (p-used)/(Number_of_blocks-i)
+  blocks_size[i] = int(np.random.poisson(theta,1))
+  used += blocks_size[i]
+blocks_size[Number_of_blocks-1]=p-used
+blocks_size=blocks_size.astype(int)
+
+
+block_idx = np.insert(np.cumsum(blocks_size),0,0)
 
 correlation_within = .9
-correlation_between = .4
+correlation_between = .1
 
-cov_mat = np.ones((ncol,ncol))*correlation_between
+cov_mat = np.ones((p,p))*correlation_between
 
 counter = 0
 for i in range(Number_of_blocks):
@@ -23,9 +36,9 @@ for i in range(Number_of_blocks):
 
 np.fill_diagonal(cov_mat,1)
 
-rv=multivariate_normal(np.zeros(ncol),cov_mat)  
+rv=multivariate_normal(np.zeros(p),cov_mat)  
 
-n=20         
+SIGNAL = np.zeros(100)         
 MW = np.zeros(100)
 ML=  np.zeros(100)
 TPL = np.zeros(100)
@@ -33,9 +46,13 @@ FPL  = np.zeros(100)
 TPW  = np.zeros(100)
 FPW  = np.zeros(100)
 
+nblocks = np.insert(np.ones(k),np.zeros(Number_of_blocks-k),0)
+print(nblocks)
+
 
 from tqdm import tqdm
 for j in tqdm(range(100)):
+   true_block = np.random.permutation(nblocks) 	
    X=rv.rvs(n)
 
    SPCA = SparsePCA(n_components=Number_of_blocks)
@@ -44,14 +61,12 @@ for j in tqdm(range(100)):
 
    XPCA = SPCA.transform(X)
 
-   signal = 2*(np.log(ncol)/n)**.5
+   signal = 2*(np.log(p)/n)**.5
 
 
-   c=1.5
+   
 
-   beta = np.zeros(ncol)
-
-   true_block = np.array([1,0,0,1,0,0,0,0,0,1])
+   beta = np.zeros(p)
 
    counter =0 
    for i in range(Number_of_blocks):
@@ -77,7 +92,7 @@ for j in tqdm(range(100)):
 
 
    beta_lasso = lasso1.coef_
-   beta_weak = np.zeros(ncol)
+   beta_weak = np.zeros(p)
    for i in range(Number_of_blocks):
      beta_weak = beta_weak + Xfit.components_[i,:]*lasso2.coef_[i]
 
@@ -86,20 +101,39 @@ for j in tqdm(range(100)):
 
    MSE_weak = (np.matmul(Xtest,beta)-np.matmul(Xtest,beta_weak))**2
    MSE_lasso = (np.matmul(Xtest,beta)-np.matmul(Xtest,beta_lasso))**2
+   SIGNAL[j] = np.average(np.matmul(Xtest,beta)**2)
    MW[j] =np.average(MSE_weak)
    ML[j] =np.average(MSE_lasso)
-   block_idx = np.insert(np.cumsum(blocks_size),0,0)  
+     
    TPW[j] = np.sum([np.sum(beta_weak[block_idx[i]:block_idx[i+1]]!=0)/blocks_size[i] for i in range(Number_of_blocks) if true_block[i]==1 ] )
    FPW[j] = np.sum([np.sum(beta_weak[block_idx[i]:block_idx[i+1]]!=0)/blocks_size[i] for i in range(Number_of_blocks) if true_block[i]==0 ] )  
  
    TPL[j] = np.sum([np.sum(beta_lasso[block_idx[i]:block_idx[i+1]]!=0)/blocks_size[i] for i in range(Number_of_blocks) if true_block[i]==1 ] )
    FPL[j] = np.sum([np.sum(beta_lasso[block_idx[i]:block_idx[i+1]]!=0)/blocks_size[i] for i in range(Number_of_blocks) if true_block[i]==0 ] )  
+ 
 
 
+print(
+"SIGNAL = "+str(np.average(SIGNAL))+"\n"+
 
-print("MSE LASSO = "+str(np.average(ML))+"\n"+ 
+"MSE LASSO = "+str(np.average(ML))+"\n"+ 
+
 "MSE WEAK = "+str(np.average(MW))+"\n"+  
 "POWER LASSO = "+str(np.average(TPL)/np.sum(true_block))+"\n"+
 "POWER WEAK = "+str(np.average(TPW)/np.sum(true_block))+"\n"+
 "FDR LASSO = "+str(np.average(FPL)/(np.average(FPL)+np.average(TPL)))+"\n"+
+
 "FDR WEAK = "+str(np.average(FPW)/(np.average(FPW)+np.average(TPW)))+"\n")
+
+np.savetxt("sig.csv",SIGNAL,delimiter =";")
+
+np.savetxt("TPL5.csv",TPL,delimiter =";")
+np.savetxt("FPL5.csv",FPL,delimiter =";")
+
+np.savetxt("TPW5.csv",TPW,delimiter =";")
+np.savetxt("FPW5.csv",FPW,delimiter =";")
+
+np.savetxt("MSEW5.csv",MW,delimiter =";")
+np.savetxt("MSEL5.csv",ML,delimiter =";")
+
+
